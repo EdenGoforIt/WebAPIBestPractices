551b12f63ee69c466ea0fe1d6f99eae9987c32f3
EdenPark <edengoforit@gmail.com>
2022-05-31 20:39:44 +1200

    Feat: Repository created and abstract classes for repository and controller

---
+++ Repository/RepositoryBase.cs
@@ -0,0 +1,48 @@
+﻿using Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected RepositoryContext Context;
+
+        protected RepositoryBase(RepositoryContext context)
+        {
+            Context = context;
+        }
+
+
+        public IQueryable<T> FindAll(bool trackChanges)
+        {
+            return !trackChanges ? Context.Set<T>().AsNoTracking() : Context.Set<T>();
+
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges)
+        {
+            return !trackChanges
+                ? Context.Set<T>().Where(expression).AsNoTracking()
+                : Context.Set<T>().Where(expression);
+        }
+
+        public void Create(T entity)
+        {
+            Context.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            Context.Set<T>().Update(entity);
+
+        }
+
+        public void Delete(T entity)
+        {
+            Context.Set<T>().Remove(entity);
+        }
+    }
+}
---
+++ WebApi-BestPractices/Controllers/BaseApiController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi_BestPractices.Controllers
+{
+    [Route("api/[controller]")]
+    public class BaseApiController : Controller
+    {
+    }
+}
