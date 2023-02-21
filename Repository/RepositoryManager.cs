dceffacd5ca317493fc682ce9ac8ae4aa388919b
EdenPark <edengoforit@gmail.com>
2023-02-22 08:38:14 +1300

    Repository Manager added

---
+++ Repository/RepositoryManager.cs
@@ -0,0 +1,22 @@
+﻿using Contracts;
+
+namespace Repository
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+        private readonly RepositoryContext _repositoryContext;
+        private ICompanyRepository _companyRepository;
+        private IEmployeeRepository _employeeRepository;
+
+        public ICompanyRepository Company => _companyRepository ??= new CompanyRepository(_repositoryContext);
+
+        public IEmployeeRepository Employe => _employeeRepository ??= new EmployeeRepository(_repositoryContext);
+
+        public RepositoryManager(RepositoryContext repository)
+        {
+            _repositoryContext = repository;
+        }
+
+        public void Save() => _repositoryContext.SaveChanges();
+    }
+}
