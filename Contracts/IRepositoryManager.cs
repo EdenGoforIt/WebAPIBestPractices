aecd732d36b8300dbc5dc26f656fe3dc49cb54bf
EdenPark <edengoforit@gmail.com>
2023-02-22 08:10:47 +1300

    IRepository manager added

---
+++ Contracts/IRepositoryManager.cs
@@ -0,0 +1,9 @@
+﻿namespace Contracts
+{
+    public interface IRepositoryManager
+    {
+        ICompanyRepository Company { get; }
+        IEmployeeRepository Employe { get; }
+        void Save();
+    }
+}
