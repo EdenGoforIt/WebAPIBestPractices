9f0bfd117ad4ff9a5e9a791ef755a7b8b62cb90b
Eden Park <edengoforit@gmail.com>
2023-10-26 21:30:55 +1300

    get company controller addeD

--- WebApi-BestPractices/Controllers/CompaniesController.cs
+++ WebApi-BestPractices/Controllers/CompaniesController.cs
@@ -1,3 +1,5 @@
+using System;
+using Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi_BestPractices.Controllers
@@ -6,6 +8,30 @@ namespace WebApi_BestPractices.Controllers
     [ApiController]
     public class CompaniesController : ControllerBase
     {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+
+        public CompaniesController(IRepositoryManager repository, ILoggerManager logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetCompanies()
+        {
+            try
+            {
+                var companies = _repository.Company.GetAllCompanies(trackChanges: false);
+
+                return Ok(companies);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the {nameof(GetCompanies)}: {ex}");
+                return StatusCode(500);
+            }
+        }
 
     }
 
