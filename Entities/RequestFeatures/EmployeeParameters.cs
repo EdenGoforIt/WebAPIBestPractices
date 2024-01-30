419bfb6f9c18b52424df9f664d6d53b709339bca
Eden Park <edengoforit@gmail.com>
2024-01-31 07:22:56 +1300

    format from rider

--- Entities/RequestFeatures/EmployeeParameters.cs
+++ Entities/RequestFeatures/EmployeeParameters.cs
@@ -1,22 +1,5 @@
 namespace Entities.RequestFeatures;
 
-public abstract class RequestParameters
-{
-    private const int MaxPageSize = 50;
-    public int PageNumber { get; set; } = 1;
-    private int _pageSize = 10;
-
-    public int PageSize
-    {
-        get => _pageSize;
-        set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
-    }
-
-    public string SearchTerm { get; set; }
-
-    public string OrderBy { get; set; }
-}
-
 public class EmployeeParameters : RequestParameters
 {
     public EmployeeParameters()
