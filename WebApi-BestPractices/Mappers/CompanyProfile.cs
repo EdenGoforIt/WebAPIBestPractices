3e8467150ec3a0ec78b45350f558500e2384aaf8
Eden Park <edengoforit@gmail.com>
2025-01-14 21:39:10 +1300

    upgraded to .net 9

--- WebApi-BestPractices/Mappers/CompanyProfile.cs
+++ WebApi-BestPractices/Mappers/CompanyProfile.cs
@@ -8,12 +8,21 @@ namespace WebApi_BestPractices.Mappers
 	{
 		public CompanyProfile()
 		{
-			CreateMap<Company, CompanyDto>().ForMember(c => c.FullAddress, opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+			CreateMap<Company, CompanyDto>()
+					.ForMember(c => c.FullAddress, opt => opt.MapFrom<FullAddressResolver>());
 			CreateMap<Employee, EmployeeDto>();
 			CreateMap<CompanyForCreationDto, Company>();
 			CreateMap<CompanyForUpdateDto, Company>();
 			CreateMap<EmployeeForCreationDto, Employee>();
 			CreateMap<EmployeeForUpdateDto, Employee>().ReverseMap();
+		}
+	}
+
+	public class FullAddressResolver : IValueResolver<Company, CompanyDto, string>
+	{
+		public string Resolve(Company source, CompanyDto destination, string destMember, ResolutionContext context)
+		{
+			return $"{source.Address} {source.Country}";
 		}
 	}
 }
