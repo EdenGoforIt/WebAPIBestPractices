41f2b3b11826cc5b0f2e807b7ba196fddab428cb
Eden Park <edengoforit@gmail.com>
2025-11-24 19:50:54 +1300

    feat: root controller added

--- WebApi-BestPractices/Controllers/RootController.cs
+++ WebApi-BestPractices/Controllers/RootController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entities.LinkModels;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
+using System.Collections.Generic;
 
 namespace WebApi_BestPractices.Controllers
 {
@@ -8,7 +10,38 @@ namespace WebApi_BestPractices.Controllers
     public class RootController(LinkGenerator linkGenerator) : ControllerBase
     {
 
+        [HttpGet(Name = "GetRoot")]
+        public IActionResult GetRoot([FromHeader(Name = "Accept")] string mediaType)
+        {
+            if (mediaType.Contains("application/vnd.apiroot"))
+            {
+                var list = new List<Link>
+                {
+                    new() {
+                        Href = linkGenerator.GetUriByName(HttpContext, "GetRoot", values: new{ })!,
+                        Rel = "self",
+                        Method = "GET"
+                    },
+                     new()
+                    {
+                        Href = linkGenerator.GetUriByName(HttpContext, "GetCompanies", new{}),
+                        Rel = "companies",
+                        Method = "GET"
+                    },
+                    new ()
+                    {
+                        Href = linkGenerator.GetUriByName(HttpContext, "CreateCompany", new {}),
+                        Rel = "create_company",
+                        Method = "POST"
+                    }
+                };
 
+                return Ok(list);
+            }
+
+            return NoContent();
+
+        }
 
     }
 }
